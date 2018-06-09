aa835db4ab10490d58992193e203e8039287037c
Archan Patkar <archanpatkar@gmail.com>
2018-06-09 16:31:52 +0530

    TODO

--- Test.js
+++ Test.js
@@ -1,5 +1,8 @@
 const { go } = require("./index")();
 
+/* TODO: Adding Channels from GO */
+
+
 console.log("Before");
-go(() => console.log("Hello from JRoutines")).then(() => console.log("Execution Complete"))
+go( () => console.log("Hello from JRoutines") ).then(() => console.log("Execution Complete"))
 console.log("After");
