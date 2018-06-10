6c0c97a9ebcd8b74031e9b45d8304c6d48276071
Archan Patkar <archanpatkar@gmail.com>
2018-06-10 12:56:08 +0530

    Temp

--- Test.js
+++ Test.js
@@ -1,16 +1,20 @@
-const { go } = require("./index")(16);
+const { go , Channel } = require("./index")(16);
+const EventEmitter = require('events');
 
 console.log("Before");
 
-function f1(x,y) {
-  console.log(`x = ${x}`);
-  console.log(`y = ${y}`);
-  return x + y;
+function f1(obj) {
+  console.log(`x = ${obj.x}`);
+  console.log(`y = ${obj.y}`);
+  return obj.x + obj.y;
 }
 
-for(let i = 0;i < 1000;i++)
-{
-  go(f1,i*10,i*10).then(result => console.log(result.value));
-}
+// Closures dont work! with Napa.js
+go(f1, { x: 10, y:10 , c: new EventEmitter() }).then(result => console.log(result.value));
+
+// for(let i = 0;i < 1000;i++)
+// {
+//   go(f1,i*10,i*10).then(result => console.log(result.value));
+// }
 
 console.log("After");
