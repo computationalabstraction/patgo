0b9cd47c13be584ebade98c57868166fcbde7d68
Archan Patkar <archanpatkar@gmail.com>
2018-06-13 01:38:44 +0530

    Update

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
 const napa = require('napajs');
 const uuidv4 = require('uuid/v4');
-const EventEmitter = require('events');
-// const providers = [ "napajs" ,  "webworker-threads"];
-
-class Channel
-{
-  constructor()
-  {
-    this.emitter = new EventEmitter();
-  }
-
-  send(...data)
-  {
-    this.emitter.emit("data",...data);
-  }
-
-  receive(handler)
-  {
-    this.emitter.on("data",handler);
-  }
-}
 
 module.exports = (size = 8) =>
 {
   const tpool = napa.zone.create( `GO_THREAD_POOL: ${uuidv4()}` , { workers: size });
-  // const store = napa.store.create('GO_STORE');
-  // const mapping = {};
-  // let counter = 0;
-
-  // Global Code Provided by the library to every Goroutine
-  // function getChannel(id)
-  // {
-  //   const store = global.napa.store.get('GO_STORE');
-  //   return store.get(id);
-  // }
-  //
-  // tpool.broadcast(getChannel.toString());
+  const registry = {};
 
   function go(task,...params)
   {
-    // if(chan)
-    // {
-    //     const id = ""+uuidv4(counter);
-    //     mapping[id] = [new Channel(), new Channel()] ;
-    //     store.set(id, mapping[id]);
-    //     return [...mapping[id],tpool.execute(task,[id])];
-    // }
-    // else
-    // {
     if(params != undefined)
     {
       return tpool.execute(task,params);
@@ -56,7 +16,6 @@ module.exports = (size = 8) =>
     {
       return tpool.execute(task);
     }
-    // }
   }
 
   function getPoolSize()
@@ -64,9 +23,32 @@ module.exports = (size = 8) =>
     return size;
   }
 
+  function register(id,func)
+  {
+    registry[id] = func;
+  }
+
+  function deregister(id)
+  {
+    delete registry[id];
+  }
+
+  function get(id)
+  {
+    return registry[id];
+  }
+
+  function addLib(code_string)
+  {
+    tpool.broadcast(code_string);
+  }
+
   return {
     go:go,
     getPoolSize:getPoolSize,
-    Channel:Channel
+    register:register,
+    deregister:deregister,
+    get:get,
+    addLib:addLib
   }
 };
