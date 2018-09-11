47fa71d7af9e4dd13f713758eecd3b3aff2e6878
archanpatkar <pat@archanpatkar.com>
2018-09-11 12:16:24 +0530

    Update

--- Test.js
+++ Test.js
@@ -2,6 +2,15 @@ const { go } = require("./index")();
 
 console.log("Before");
 
-go( () => console.log("Executed Concurrently") ).then( () => console.log("Execution Complete") );
+async function testing()
+{
+    let val = await go(
+        (...val) => val.reduce((acc,v) => acc + v,0),
+        [10,20,30,40,50]
+    );
+    console.log("Execution Complete Value = " + val);
+}
+
+testing();
 
 console.log("After");
