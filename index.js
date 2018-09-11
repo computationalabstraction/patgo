69553408cabfb5dc99078163f0d6ba6c5b561656
archanpatkar <pat@archanpatkar.com>
2018-09-11 17:37:46 +0530

    Update

--- index.js
+++
@@ -1,8 +0,0 @@
-// const napa = require("./napa.backend");
-const node = require("./node.backend");
-
-module.exports = (backend = "node" , size = 8) =>
-{ 
-  // if(backend == "napa") return napa(size);
-  if(backend == "node") return node();
-};
