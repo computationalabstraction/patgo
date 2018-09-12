99e92bc65389b9830d599d90b9916ff29a80a9b3
archanpatkar <pat@archanpatkar.com>
2018-09-12 12:26:20 +0530

    Update

--- src/worker.js
+++ src/worker.js
@@ -1,10 +1,38 @@
 const { parentPort , workerData } = require('worker_threads');
+
+function send(data)
+{
+    return new Promise( (resolve,reject) => {
+        try {
+            this.postMessage(data);   
+            resolve(true);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+function receive()
+{
+    return new Promise( (resolve,reject) => {
+        try {
+            this.on('message', data => {
+                resolve(data);
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
 let context = JSON.parse(workerData);
 parentPort.on("message", async packet => {
     eval(packet.code);
     let data;
     try
     {
+        packet.channel.send = send;
+        packet.channel.receive = receive;
         data = await routine(packet.channel);
     }
     catch(err)
