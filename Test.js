0203d8d62c8fe46f90677b5d64e2fffaf93881ab
archanpatkar <pat@archanpatkar.com>
2019-10-04 20:02:55 +0530

    Testing lifecycle

--- Test.js
+++ Test.js
@@ -23,10 +23,17 @@ console.log("Before");
     
     console.log(msg);
 
-    let add = stay((x,y) => x+y);
-    add(10,20).then(v => {console.log("First"); console.log(v)});
-    add(50,60).then(v => {console.log("Second"); console.log(v)});
-    add(100,200).then(v => {console.log("Second"); console.log(v)});
+    let add = stay(async (x,y,c) => { 
+        console.log(await c.receive());
+        c.send("Goodbye!");
+        return x+y;
+    });
+    let p = add(10,20);
+    p.channel.send("Hello");
+    console.log(await p.channel.receive());
+    console.log(await p);
+    // add(50,60).then(v => {console.log("Second"); console.log(v)});
+    // add(100,200).then(v => {console.log("Third"); console.log(v)});
 })();
 
 // frame.p.then((e) => console.log("Done"));
